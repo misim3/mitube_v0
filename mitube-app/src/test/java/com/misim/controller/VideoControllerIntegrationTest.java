fb34c0c01ab567ca16846c845bc32a66f7d15147
misim3 <misim@student.42seoul.kr>
2024-05-02 14:54:15 +0900

    refactor: 테스트 주석처리 취소

--- mitube-app/src/test/java/com/misim/controller/VideoControllerIntegrationTest.java
+++ mitube-app/src/test/java/com/misim/controller/VideoControllerIntegrationTest.java
@@ -312,8 +312,6 @@ class VideoControllerIntegrationTest {
                 .param("videoId", String.valueOf(videoId))
                 .param("userId", String.valueOf(userId))
                 .contentType(MediaType.APPLICATION_JSON));
-
-        System.out.println(watchingInfoRepository.count());
     }
 
     @Test
