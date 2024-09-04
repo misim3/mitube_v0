b36f07d02284b9bbb8d65d0479217630359798ac
misim3 <sim000314@naver.com>
2024-09-05 02:14:24 +0900

    refactor: 성능 테스트를 위한 수정

--- mitube-app/src/main/java/com/misim/util/WatchingInfoScheduler.java
+++ mitube-app/src/main/java/com/misim/util/WatchingInfoScheduler.java
@@ -1,18 +1,18 @@
-package com.misim.util;
-
-import com.misim.service.WatchingInfoService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class WatchingInfoScheduler {
-
-    private final WatchingInfoService watchingInfoService;
-
-    @Scheduled(fixedRate = 60000)  // 5분 간격
-    public void scheduledUpdateWatchingInfo() {
-        watchingInfoService.scheduleUpdate();
-    }
-}
+//package com.misim.util;
+//
+//import com.misim.service.WatchingInfoService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class WatchingInfoScheduler {
+//
+//    private final WatchingInfoService watchingInfoService;
+//
+//    @Scheduled(fixedRate = 10000)  // 10초 간격
+//    public void scheduledUpdateWatchingInfo() {
+//        watchingInfoService.scheduleUpdate();
+//    }
+//}
