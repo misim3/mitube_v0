f4c36aa8568547ea65180412e2757b5965190329
misim3 <sim000314@naver.com>
2024-10-09 16:39:08 +0900

    refactor: 가독성을 위한 swagger 어노테이션 삭제

--- mitube-app/src/main/java/com/misim/controller/VideoController.java
+++ mitube-app/src/main/java/com/misim/controller/VideoController.java
@@ -4,14 +4,6 @@ import com.misim.controller.model.Request.CreateVideoRequest;
 import com.misim.controller.model.Response.WatchingVideoResponse;
 import com.misim.exception.CommonResponse;
 import com.misim.service.VideoService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,20 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "동영상 API", description = "동영상 정보 관련 API")
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/videos")
 public class VideoController {
 
     private final VideoService videoService;
 
-    @Operation(summary = "동영상 생성", description = "새로운 동영상을 생성합니다.")
-    @Parameter(name = "VideoDto", description = "Video 생성을 위한 데이터")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "동영상 생성 성공"),
-        @ApiResponse(responseCode = "400", description = "요청 형식이 올바르지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
-    })
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
     @PostMapping("/create")
     public void createVideos(@RequestBody CreateVideoRequest createVideoRequest) {
 
@@ -44,14 +32,8 @@ public class VideoController {
         videoService.createVideos(createVideoRequest);
     }
 
-    @Operation(summary = "동영상 시청", description = "동영상 시청을 시작합니다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "동영상 시청 요청 성공"),
-        @ApiResponse(responseCode = "400", description = "요청 형식이 올바르지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
-    })
     @GetMapping("/{videoId}")
-    public CommonResponse<WatchingVideoResponse> startWatchingVideo(
-        @PathVariable @Parameter(name = "videoId", description = "시청할 동영상 식별 정보", required = true) Long videoId) {
+    public CommonResponse<WatchingVideoResponse> startWatchingVideo(@PathVariable Long videoId) {
 
         WatchingVideoResponse response = videoService.startWatchingVideo(videoId);
 
@@ -61,30 +43,22 @@ public class VideoController {
             .build();
     }
 
-    @Operation(summary = "동영상 시청 중", description = "유저가 동영상을 계속 시청 중인지 확인하여 동영상 시청 정보를 업데이트합니다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "동영상 시청 정보 업데이트 성공"),
-        @ApiResponse(responseCode = "400", description = "요청 형식이 올바르지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
-    })
     @PostMapping("/{videoId}/update")
     public void watchingVideo(
-        @PathVariable @Parameter(name = "videoId", description = "시청할 동영상 식별 정보", required = true) Long videoId,
-        @RequestParam @Parameter(name = "userId", description = "동영상을 시청할 유저의 식별 정보") Long userId,
-        @RequestParam Long watchingTime) {
+        @PathVariable Long videoId,
+        @RequestParam Long userId,
+        @RequestParam Long watchingTime
+    ) {
 
         videoService.updateWatchingVideo(videoId, userId, watchingTime);
     }
 
-    @Operation(summary = "동영상 시청 완료", description = "동영상 시청 완료로 시청 정보를 업데이트합니다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "동영상 시청 정보 업데이트 성공"),
-        @ApiResponse(responseCode = "400", description = "요청 형식이 올바르지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
-    })
     @PostMapping("/{videoId}/complete")
     public void completeWatchingVideo(
-        @PathVariable @Parameter(name = "videoId", description = "시청할 동영상 식별 정보", required = true) Long videoId,
-        @RequestParam @Parameter(name = "userId", description = "동영상을 시청할 유저의 식별 정보") Long userId,
-        @RequestParam Long watchingTime) {
+        @PathVariable Long videoId,
+        @RequestParam Long userId,
+        @RequestParam Long watchingTime
+    ) {
 
         videoService.completeWatchingVideo(videoId, userId, watchingTime);
     }
