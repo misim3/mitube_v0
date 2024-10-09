81ea8fa9964113c8c99d1a5e76bec530e22e0979
misim3 <sim000314@naver.com>
2024-10-09 15:18:14 +0900

    refactor: 임시 시청 정보 엔티티 삭제

--- mitube-app/src/main/java/com/misim/service/WatchingInfoService.java
+++ mitube-app/src/main/java/com/misim/service/WatchingInfoService.java
@@ -1,7 +1,6 @@
 package com.misim.service;
 
 import com.misim.entity.WatchingInfo;
-import com.misim.repository.TempWatchingInfoRepository;
 import com.misim.repository.WatchingInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
@@ -12,35 +11,9 @@ import org.springframework.stereotype.Service;
 public class WatchingInfoService {
 
     private final WatchingInfoRepository watchingInfoRepository;
-    private final TempWatchingInfoRepository tempWatchingInfoRepository;
 
     @Async
     public void updateWatchingInfo(Long videoId, Long userId, Long watchingTime) {
-
-//        TempWatchingInfo temp;
-//        if (tempWatchingInfoRepository.existsByUserIdAndVideoId(userId, videoId)) {
-//
-//            temp = tempWatchingInfoRepository.findByUserIdAndVideoId(userId, videoId);
-//
-//        } else if (watchingInfoRepository.existsByUserIdAndVideoId(userId, videoId)) {
-//
-//            WatchingInfo watchingInfo = watchingInfoRepository.findByUserIdAndVideoId(userId, videoId);
-//
-//            temp = watchingInfo.convertToTempWatchingInfo();
-//        } else {
-//
-//            WatchingInfo watchingInfo = WatchingInfo.builder()
-//                .userId(userId)
-//                .videoId(videoId)
-//                .watchingTime(watchingTime)
-//                .build();
-//
-//            temp = watchingInfoRepository.save(watchingInfo).convertToTempWatchingInfo();
-//        }
-//
-//        temp.setWatchingTime(watchingTime);
-//
-//        tempWatchingInfoRepository.save(temp);
 
         WatchingInfo watchingInfo;
         if (watchingInfoRepository.existsByUserIdAndVideoId(userId, videoId)) {
@@ -66,15 +39,4 @@ public class WatchingInfoService {
 
         watchingInfoRepository.save(watchingInfo);
     }
-
-//    @Transactional
-//    public void scheduleUpdate() {
-//
-//        watchingInfoRepository.saveAll(tempWatchingInfoRepository.findAll()
-//            .stream()
-//            .map(TempWatchingInfo::convertToWatchingInfo).toList()
-//        );
-//
-//        tempWatchingInfoRepository.deleteAll();
-//    }
 }
