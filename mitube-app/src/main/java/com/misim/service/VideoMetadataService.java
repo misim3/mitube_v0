d43751bf52f33bbc0d5a396e6ee520b5b4ec7198
misim3 <sim000314@naver.com>
2024-10-29 21:42:45 +0900

    feat: 영상 메타데이터 레포지토리, 서비스 생성

---
+++ mitube-app/src/main/java/com/misim/service/VideoMetadataService.java
@@ -0,0 +1,95 @@
+package com.misim.service;
+
+import com.misim.entity.VideoMetadata;
+import com.misim.repository.VideoMetadataRepository;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VideoMetadataService {
+
+    private final VideoMetadataRepository videoMetadataRepository;
+
+    public VideoMetadata create() {
+
+        VideoMetadata metadata = VideoMetadata.builder()
+            .viewCount(0L)
+            .likeCount(0L)
+            .dislikeCount(0L)
+            .build();
+
+        return videoMetadataRepository.save(metadata);
+    }
+
+    public VideoMetadata read(Long id) {
+
+        return videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new);
+    }
+
+    public Long readViewCount(Long id) {
+
+        return videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new)
+            .getViewCount();
+    }
+
+    public Long readLikeCount(Long id) {
+
+        return videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new)
+            .getLikeCount();
+    }
+
+    public Long readDislikeCount(Long id) {
+
+        return videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new)
+            .getDislikeCount();
+    }
+
+    public void updateViewCount(Long id) {
+
+        VideoMetadata metadata = videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new);
+
+        metadata.incrementViewCount();
+
+        videoMetadataRepository.save(metadata);
+    }
+
+    public void updateLikeCount(Long id, boolean isChecked) {
+
+        VideoMetadata metadata = videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new);
+
+        if (isChecked) {
+            metadata.incrementLikeCount();
+        } else {
+            metadata.decrementLikeCount();
+        }
+
+        videoMetadataRepository.save(metadata);
+    }
+
+    public void updateDislikeCount(Long id, boolean isChecked) {
+
+        VideoMetadata metadata = videoMetadataRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new);
+
+        if (isChecked) {
+            metadata.incrementDislikeCount();
+        } else {
+            metadata.decrementDislikeCount();
+        }
+
+        videoMetadataRepository.save(metadata);
+    }
+
+    public void delete(Long id) {
+
+        videoMetadataRepository.deleteById(id);
+    }
+}
