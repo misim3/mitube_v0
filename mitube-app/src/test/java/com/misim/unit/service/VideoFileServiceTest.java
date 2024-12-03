a810a837b411acd43f41aac90ae30ceeda78f377
misim3 <sim000314@naver.com>
2024-12-03 22:19:44 +0900

    refactor: 테스트 메서드 수정

--- mitube-app/src/test/java/com/misim/unit/service/VideoFileServiceTest.java
+++ mitube-app/src/test/java/com/misim/unit/service/VideoFileServiceTest.java
@@ -1,25 +1,30 @@
 package com.misim.unit.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import com.misim.entity.VideoFile;
+import com.misim.exception.MitubeException;
 import com.misim.repository.VideoFileRepository;
 import com.misim.service.VideoFileService;
 import com.misim.util.Base64Convertor;
+import com.misim.util.TimeUtil;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -39,56 +44,118 @@ class VideoFileServiceTest {
     private MultipartFile mockMultipartFile;
 
     @Mock
-    private FileSystemResource mockFileSystemResource;
-
-    @Mock
     private Path mockPath;
 
+    @Mock
+    private LocalDateTime mockDateTime;
 
     private static final Long EXISTING_VIDEO_FILE_ID = 1L;
     private static final Long NON_EXISTENT_VIDEO_FILE_ID = 99999L;
 
-    @BeforeEach
-    void setUp() {
-        ReflectionTestUtils.setField(videoFileService, "UPLOAD_PATH", "mock/upload/path");
-    }
+//    @BeforeEach
+//    void setUp() {
+//        ReflectionTestUtils.setField(videoFileService, "UPLOAD_PATH", "mock/upload/path");
+//    }
 
     @Test
-    void uploadVideo_shouldSaveFileAndReturnEncodedId() throws IOException, NoSuchFieldException, IllegalAccessException {
+    void uploadVideo_shouldSaveFileAndReturnEncodedId() throws IOException {
 
         String originalFilename = "test.mp4";
 
-        Mockito.mockStatic(Paths.class);
-        Mockito.when(Paths.get(anyString())).thenReturn(mockPath);
-        Mockito.mockStatic(Files.class);
-        Mockito.when(Files.exists(mockPath)).thenReturn(false);
-        Mockito.when(Files.createDirectories(any(Path.class))).thenReturn(mockPath);
+        try (MockedStatic<TimeUtil> timeUtilMock = mockStatic(TimeUtil.class);
+            MockedStatic<Paths> pathsMock = mockStatic(Paths.class);
+            MockedStatic<Files> filesMock = mockStatic(Files.class);
+            MockedStatic<Base64Convertor> base64ConvertorMock = mockStatic(Base64Convertor.class)
+        ) {
 
-        Mockito.when(mockMultipartFile.getOriginalFilename()).thenReturn(originalFilename);
+            when(TimeUtil.getNow()).thenReturn(mockDateTime);
 
-        Mockito.doNothing().when(mockMultipartFile).transferTo(any(Path.class));
+            when(Paths.get(anyString())).thenReturn(mockPath);
 
-        Mockito.when(videoFileRepository.save(any(VideoFile.class))).thenReturn(mockVideoFile);
+            when(Files.exists(mockPath)).thenReturn(false);
+            when(Files.createDirectories(any(Path.class))).thenReturn(mockPath);
 
-        Mockito.when(mockVideoFile.getId()).thenReturn(1L);
-        Mockito.mockStatic(Base64Convertor.class);
-        Mockito.when(Base64Convertor.encode(anyLong())).thenReturn(String.valueOf(1L));
+            when(mockMultipartFile.getOriginalFilename()).thenReturn(originalFilename);
 
-        String encodedId = videoFileService.uploadVideo(mockMultipartFile);
+            doNothing().when(mockMultipartFile).transferTo(any(Path.class));
+
+            when(videoFileRepository.save(any(VideoFile.class))).thenReturn(mockVideoFile);
+
+            when(mockVideoFile.getId()).thenReturn(1L);
+
+            when(Base64Convertor.encode(anyLong())).thenReturn(String.valueOf(1L));
+
+            String encodedId = videoFileService.uploadVideo(mockMultipartFile);
+
+            verify(mockMultipartFile, times(1)).getOriginalFilename();
+            verify(mockMultipartFile, times(1)).transferTo(any(Path.class));
+            verify(videoFileRepository, times(1)).save(any(VideoFile.class));
+            verify(mockVideoFile, times(1)).getId();
+            assertThat(encodedId).isEqualTo(String.valueOf(1L));
+
+        }
+
+    }
+
+    @Test
+    void uploadVideo_shouldThrowException_whenFailToCreateDirectories() throws IOException {
+
+        try (MockedStatic<TimeUtil> timeUtilMock = mockStatic(TimeUtil.class);
+            MockedStatic<Paths> pathsMock = mockStatic(Paths.class);
+            MockedStatic<Files> filesMock = mockStatic(Files.class)
+        ) {
+            when(TimeUtil.getNow()).thenReturn(mockDateTime);
+            when(Paths.get(anyString())).thenReturn(mockPath);
+            when(Files.exists(mockPath)).thenReturn(false);
+            when(Files.createDirectories(any(Path.class))).thenThrow(IOException.class);
+
+            assertThrows(MitubeException.class, () -> videoFileService.uploadVideo(mockMultipartFile));
+
+        }
+
+    }
+
+    @Test
+    void uploadVideo_shouldThrowException_whenFailToTransferFile() throws IOException {
+
+        String originalFilename = "test.mp4";
+
+        mockStatic(TimeUtil.class);
+        when(TimeUtil.getNow()).thenReturn(mockDateTime);
+        mockStatic(Paths.class);
+        when(Paths.get(anyString())).thenReturn(mockPath);
+        mockStatic(Files.class);
+        when(Files.exists(mockPath)).thenReturn(false);
+        when(Files.createDirectories(any(Path.class))).thenReturn(mockPath);
+
+        when(mockMultipartFile.getOriginalFilename()).thenReturn(originalFilename);
+
+        doThrow(IOException.class).when(mockMultipartFile).transferTo(mockPath);
+
+        assertThrows(MitubeException.class, () -> videoFileService.uploadVideo(mockMultipartFile));
 
         verify(mockMultipartFile, times(1)).getOriginalFilename();
         verify(mockMultipartFile, times(1)).transferTo(any(Path.class));
-        verify(videoFileRepository, times(1)).save(any(VideoFile.class));
-        verify(mockVideoFile, times(1)).getId();
-        assertThat(encodedId).isEqualTo(String.valueOf(1L));
 
     }
 
     @Test
-    void getFileResource() {
+    void getFileResource_shouldReturnResource() {
+
+        String path = "test_path";
+
+        Resource resource = videoFileService.getFileResource(path);
+
+        assertThat(resource).isNotNull();
+
     }
 
     @Test
-    void deleteVideoFileById() {
+    void deleteVideoFileById_shouldDelete_whenIdExists() {
+
+        when(videoFileRepository.findById(EXISTING_VIDEO_FILE_ID)).thenReturn(
+            Optional.of(mockVideoFile));
+
+
     }
 }
